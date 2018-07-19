ae8866b4ce71c6398be2ad1714c39d7bb7c5d828
Phong Tran <tt.phong2008>
2018-07-20 01:09:40 +0700

    Testscript 1 - step 4

--- src/selenium_api/Topic_02_Xpath_Css_Locator.java
+++ src/selenium_api/Topic_02_Xpath_Css_Locator.java
@@ -3,7 +3,10 @@ package selenium_api;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 
 public class Topic_02_Xpath_Css_Locator {
 	WebDriver driver;
@@ -17,6 +20,17 @@ public class Topic_02_Xpath_Css_Locator {
 	  //tagname
 	  //Xpath
 	  //Css selector
+  }
+  
+  @Test
+  public void TC_01_VerifyURL_Title() throws InterruptedException {
+	  driver = new FirefoxDriver();
+	  driver.get("http://live.guru99.com");
+	  Assert.assertEquals(driver.getTitle(), "Home page");
+	  
+	  driver.findElement(By.linkText("MY ACCOUNT")).click();
+	  Thread.sleep(2000);
+	  driver.findElement(By.xpath(".//*[@id='login-form']/div/div[1]/div[2]/a")).click();
   }
   @BeforeClass
   public void beforeClass() {
