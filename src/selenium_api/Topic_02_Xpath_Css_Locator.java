1c63577fcecc3fdfe15cc9961f7f05ef1c25e74f
phongtran <phongtran@192.168.3.4>
2018-07-19 16:35:01 +0700

    XPath Css

---
+++ src/selenium_api/Topic_02_Xpath_Css_Locator.java
@@ -0,0 +1,29 @@
+package selenium_api;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.openqa.selenium.WebDriver;
+
+public class Topic_02_Xpath_Css_Locator {
+	WebDriver driver;
+  @Test
+  public void f() {
+	  //ID
+	  //Class
+	  //Name
+	  //Linktext
+	  //Partial linktext
+	  //tagname
+	  //Xpath
+	  //Css selector
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
